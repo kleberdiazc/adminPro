05674e1da184d29a808ad55773ab06a8eb390271
kleberdiazc <kleberstevn-diaz@hotmail.com>
2019-09-09 10:50:12 -0500

    Modificaciones en rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
 import { RegisterComponent } from './login/register.component';
 import { PagesModule } from './pages/pages.module';
+import { SharedModule } from './shared/shared.module';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumsComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    PagesModule
+    PagesModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
